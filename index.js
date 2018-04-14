1c581c36bff7ea5777f797f6d329d99d48b9a934
Ke Wang <453587854@qq.com>
2018-04-14 20:39:01 +0800

    sync 文件到bucket

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const syncBatch = require("./methods/syncBatch");
 
 const meta = readMeta();
 const config = readConfig();
-syncBatch(config.canonical_server, meta.last_sync_ts);
+syncBatch();
--- methods/syncBatch.js
+++ methods/syncBatch.js
@@ -1,14 +1,21 @@
 const urljoin = require('url-join');
 const request = require('request');
+const fs = require('fs');
+const path = require('path');
+const http = require('http');
+const readConfig = require("./readConfig");
+const readMeta = require("./readMeta");
 
-module.exports = function syncBatch(server_url, last_sync_ts, batch_number = 0) {
+module.exports = function syncBatch(batch_number = 0) {
+  let config = readConfig();
+  let meta = readMeta();
   return new Promise(((resolve, reject) => {
-    let endpoint = urljoin(server_url, 'sync');
+    let endpoint = urljoin(config.canonical_server, 'sync');
     request({
       url: endpoint,
       method: "POST",
       body: {
-        last_sync_ts,
+        last_sync_ts: meta.last_sync_ts,
         batch_number,
       },
       json: true,
@@ -24,6 +31,19 @@ module.exports = function syncBatch(server_url, last_sync_ts, batch_number = 0) 
       let sql = [];
       tables.forEach(tableName => {
         body[tableName].forEach(record => {
+
+          // 下载之前没有下载过的文件包
+          if (record.code_bundle_hash) {
+            let filePath = path.join(config.file_bucket_path, record.code_bundle_hash);
+            if (!fs.existsSync(filePath)) {
+              let file = fs.createWriteStream(filePath);
+              http.get(urljoin(config.canonical_server, 'file/download?hash=' + record.code_bundle_hash), function (response) {
+                response.pipe(file);
+                //TODO: promise cannot resolve unless all downloads are done.
+              });
+            }
+          }
+
           sql.push(`INSERT INTO ${tableName}` +
             ` (${Object.keys(record).join(',')}) values` +
             ` (${Object.keys(record).map(_ => `"${typeof record[_] === 'string' ? record[_].replace(/([\\"])/g, "\\$1") : record[_]}"`).join(',')})` +
