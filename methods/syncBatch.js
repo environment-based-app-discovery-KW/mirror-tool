f313bfc0d2bc78b8be5714108e4505959bed4db6
Ke Wang <453587854@qq.com>
2018-04-14 20:31:27 +0800

    generate INSERT sql from data

--- methods/syncBatch.js
+++ methods/syncBatch.js
@@ -18,7 +18,20 @@ module.exports = function syncBatch(server_url, last_sync_ts, batch_number = 0) 
         return;
       }
 
-      console.log(body);
+      let itemsPerBatch = body.$items_per_batch;
+      let tables = Object.keys(body).filter(_ => !_.startsWith('$'));
+      let syncFinished = tables.every(_ => body[_].length < itemsPerBatch);
+      let sql = [];
+      tables.forEach(tableName => {
+        body[tableName].forEach(record => {
+          sql.push(`INSERT INTO ${tableName}` +
+            ` (${Object.keys(record).join(',')}) values` +
+            ` (${Object.keys(record).map(_ => `"${typeof record[_] === 'string' ? record[_].replace(/([\\"])/g, "\\$1") : record[_]}"`).join(',')})` +
+            ` ON DUPLICATE KEY UPDATE`)
+          //TODO: 一个INSERT，多个values tuple，效率更高
+        });
+      });
+      console.log(sql.join('\n'));
       //TODO: sync response to db, save file to path
     });
   }));
