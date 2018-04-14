5ea6055017e411be1b46cfe968c29206da6be680
Ke Wang <453587854@qq.com>
2018-04-14 21:09:54 +0800

    sync all batches

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const readMeta = require("./methods/readMeta");
-const readConfig = require("./methods/readConfig");
 const syncBatch = require("./methods/syncBatch");
 
-const meta = readMeta();
-const config = readConfig();
-syncBatch().then(_ => {
-  console.log("@syncBatch", _);
-});
+(async () => {
+  let batchNumber = 0;
+  while (true) {
+    let syncResult = await syncBatch(batchNumber);
+    if (syncResult.syncFinished) {
+      break;
+    }
+    batchNumber++;
+  }
+  console.log("\nall done.");
+})();
