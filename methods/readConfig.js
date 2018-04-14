d512dab9f136e42ae9f71a4e740f9106aa0a9098
Ke Wang <453587854@qq.com>
2018-04-14 20:40:46 +0800

    lodash memoize config

--- methods/readConfig.js
+++ methods/readConfig.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
+const _ = require('lodash');
 const configFilePath = 'config.json';
 
-module.exports = function readConfig() {
+module.exports = _.memoize(function readConfig() {
   return JSON.parse(fs.readFileSync(configFilePath, { encoding: "utf8" }));
-};
+});
 
